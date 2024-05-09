36472d07426f42243fe7481d5211bea2b1f8d289
Jessica Gisela Amaral Franco <a01640373@tec.mx>
2024-05-08 22:10:40 -0600

    Pedro

---
+++ lib/main.dart
@@ -0,0 +1,314 @@
+import 'dart:developer';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.redAccent, // Cambio de color a rojo
+    ),
+  );
+  runApp(const YoutubePlayerDemoApp());
+}
+
+class YoutubePlayerDemoApp extends StatelessWidget {
+  const YoutubePlayerDemoApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Youtube Player Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.red, // Cambio de color a rojo
+        appBarTheme: const AppBarTheme(
+          color: Colors.redAccent, // Cambio de color a rojo
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w300,
+            fontSize: 20,
+          ),
+        ),
+        iconTheme: const IconThemeData(
+          color: Colors.redAccent, // Cambio de color a rojo
+        ),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late YoutubePlayerController _controller;
+  late TextEditingController _idController;
+  late TextEditingController _seekToController;
+
+  late YoutubeMetaData _videoMetaData;
+  double _volume = 100;
+  bool _muted = false;
+  bool _isPlayerReady = false;
+
+  final List<String> _ids = [
+    YoutubePlayer.convertUrlToId(
+            "https://www.youtube.com/watch?v=VBwxknMQJWE&pp=ygUcdm9taXRhIHBvcnF1ZSBuYWRpZSBtZSBheXVkYQ%3D%3D")
+        .toString(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+      initialVideoId: _ids.first,
+      flags: const YoutubePlayerFlags(
+        mute: false,
+        autoPlay: true,
+        disableDragSeek: false,
+        loop: false,
+        isLive: false,
+        forceHD: false,
+        enableCaption: true,
+      ),
+    )..addListener(listener);
+    _idController = TextEditingController();
+    _seekToController = TextEditingController();
+    _videoMetaData = const YoutubeMetaData();
+  }
+
+  void listener() {
+    if (_isPlayerReady && mounted && !_controller.value.isFullScreen) {
+      setState(() {
+        _videoMetaData = _controller.metadata;
+      });
+    }
+  }
+
+  @override
+  void deactivate() {
+    // Pauses video while navigating to next page.
+    _controller.pause();
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _idController.dispose();
+    _seekToController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Perdón, Rafa. Esta tarea me mató :(',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            height: 200, // Adjust height as needed
+            color: Colors.red, // Cambio de color a rojo
+            child: YoutubePlayer(
+              controller: _controller,
+              showVideoProgressIndicator: true,
+              progressIndicatorColor:
+                  Colors.redAccent, // Cambio de color a rojo
+              topActions: <Widget>[
+                const SizedBox(width: 8.0),
+                Expanded(
+                  child: Text(
+                    _controller.metadata.title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.settings,
+                    color: Colors.white,
+                    size: 25.0,
+                  ),
+                  onPressed: () {
+                    log('Settings Tapped!');
+                  },
+                ),
+              ],
+              onReady: () {
+                _isPlayerReady = true;
+              },
+              onEnded: (data) {
+                _controller
+                    .load(_ids[(_ids.indexOf(data.videoId) + 1) % _ids.length]);
+                _showSnackBar('Next Video Started!');
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                _space,
+                _text('Title', _videoMetaData.title),
+                _space,
+                _text('Channel', _videoMetaData.author),
+                _space,
+                _text('Video Id', _videoMetaData.videoId),
+                _space,
+                Row(
+                  children: [
+                    _text(
+                      'Playback Quality',
+                      _controller.value.playbackQuality ?? '',
+                    ),
+                    const Spacer(),
+                  ],
+                ),
+                _space,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        _controller.value.isPlaying
+                            ? Icons.pause
+                            : Icons.play_arrow,
+                      ),
+                      onPressed: _isPlayerReady
+                          ? () {
+                              _controller.value.isPlaying
+                                  ? _controller.pause()
+                                  : _controller.play();
+                              setState(() {});
+                            }
+                          : null,
+                    ),
+                    IconButton(
+                      icon: Icon(_muted ? Icons.volume_off : Icons.volume_up),
+                      onPressed: _isPlayerReady
+                          ? () {
+                              _muted
+                                  ? _controller.unMute()
+                                  : _controller.mute();
+                              setState(() {
+                                _muted = !_muted;
+                              });
+                            }
+                          : null,
+                    ),
+                    FullScreenButton(
+                      controller: _controller,
+                      color: Colors.redAccent, // Cambio de color a rojo
+                    ),
+                  ],
+                ),
+                _space,
+                Row(
+                  children: <Widget>[
+                    const Text(
+                      "Volume",
+                      style: TextStyle(fontWeight: FontWeight.w300),
+                    ),
+                    Expanded(
+                      child: Slider(
+                        inactiveColor: Colors.transparent,
+                        value: _volume,
+                        min: 0.0,
+                        max: 100.0,
+                        divisions: 10,
+                        label: '${(_volume).round()}',
+                        onChanged: _isPlayerReady
+                            ? (value) {
+                                setState(() {
+                                  _volume = value;
+                                });
+                                _controller.setVolume(_volume.round());
+                              }
+                            : null,
+                      ),
+                    ),
+                  ],
+                ),
+                _space,
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            color: Colors.redAccent, // Cambio de color a rojo
+            child: Text(
+              'Perdón, Rafa. Esta tarea me mató',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _text(String title, String value) {
+    return RichText(
+      text: TextSpan(
+        text: '$title : ',
+        style: const TextStyle(
+          color: Colors.redAccent, // Cambio de color a rojo
+          fontWeight: FontWeight.bold,
+        ),
+        children: [
+          TextSpan(
+            text: value,
+            style: const TextStyle(
+              color: Colors.redAccent, // Cambio de color a rojo
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget get _space => const SizedBox(height: 10);
+
+  void _showSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          message,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontWeight: FontWeight.w300,
+            fontSize: 16.0,
+          ),
+        ),
+        backgroundColor: Colors.redAccent, // Cambio de color a rojo
+        behavior: SnackBarBehavior.floating,
+        elevation: 1.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50.0),
+        ),
+      ),
+    );
+  }
+}
